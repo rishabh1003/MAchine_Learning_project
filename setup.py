4818185226e07738f6b17fda85947cf66ef40760
Rishabh Gupta <gupta.rishabh10mar2001@gmail.com>
2022-07-19 20:55:48 +0530

    update

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
-from setuptools import setup
-from typing import List 
+from setuptools import setup,find_packages
+from typing import List
 
-def get_requirements_list()->List[str]:
-    """ Description: This function is going to return list of 
-    requirements mention in requirements.txt file 
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="Rishabh Gupta"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+
+
+def get_requirements_list() -> List[str]:
     """
-    with open("requirements.txt") as requirement_file:
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
         return requirement_file.readlines()
+    
+
 
 
 setup(
-    name="housing-predictor",
-    version="0.0.1",
-    author="Rishabh Gupta",
-    packages=["housing"],
-    install_requires=get_requirements_list()
-
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(),
+install_requires=get_requirements_list()
 )
 
-
-if __name__=="__main__":
-    print(get_requirements_list())
