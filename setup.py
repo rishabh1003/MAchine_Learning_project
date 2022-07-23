f578fd5abd59ac8addaaa58da68c672f2a92a887
Rishabh Gupta <gupta.rishabh10mar2001@gmail.com>
2022-07-23 23:10:59 +0530

    pipeline

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from typing import List
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
 VERSION="0.0.3"
-AUTHOR="Rishabh Gupta"
+AUTHOR="Avnish Yadav"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-
+HYPHEN_E_DOT = "-e ."
 
 
 def get_requirements_list() -> List[str]:
@@ -20,8 +20,11 @@ def get_requirements_list() -> List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
-    
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 
 
@@ -30,7 +33,6 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=find_packages(),
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
-
